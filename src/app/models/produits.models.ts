bfe93cbbfd5aa33edbdf0f3b69664d0bb30b692c
Penda KA <endap95@gmail.com>
2023-02-09 10:44:15 +0000

    fistcatalogue

--- src/app/models/produits.models.ts
+++ src/app/models/produits.models.ts
@@ -2,4 +2,5 @@ export  interface    Produit {
     id : number;
     name: string;
     price: number
+    promotion:boolean
 }
--- src/app/services/produits.service.ts
+++ src/app/services/produits.service.ts
@@ -10,11 +10,11 @@ export class ProduitsService {
 private produits! : Array<Produit>
   constructor() { 
     this.produits = [
-      {id: 1 , name: " computer", price: 25000},
-      {id: 2 , name: " printer", price: 75000},
-      {id: 3 , name: " phone", price: 5000},
-      {id: 4 , name: " desktop", price: 35000},
-      {id: 5 , name: " came", price: 45000},
+      {id: 1 , name: " computer", price: 25000, promotion:true},
+      {id: 2 , name: " printer", price: 75000, promotion:true },
+      {id: 3 , name: " phone", price: 5000,promotion:true },
+      {id: 4 , name: " desktop", price: 35000,promotion:false},
+      {id: 5 , name: " came", price: 45000,promotion:true},
     ];
   }
   // gestion de l'asynchrone avec Observable
